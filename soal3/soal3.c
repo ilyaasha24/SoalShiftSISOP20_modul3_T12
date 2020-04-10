b7b418c0e052c28161d7d6e65adf586a8c01c3e7
Ilya Asha <ilyaasha24@gmail.com>
2020-04-10 20:01:17 +0700

    Update soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -1,45 +1,76 @@
-#include <errno.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include <dirent.h>
+#include <ctype.h>
 
-static void mkfolder(const char *dir) {
-	char *ext = strrchr(dir, '.');
- 	mkdir(dir, 0755);
-}
-static void mv(const char *file) {
+void mv(char *file) {
   char *ext = strrchr(file, '.');
-  if(!ext || ext == file) {
-    /* code */
-  } else {
-    mkfolder(ext + 1);
-    size_t len = strlen(ext + 1) + strlen(file) + sizeof("/");
-    char *tmp = malloc(len);
-    sprintf(tmp, "%s/%s", ext + 1, file);
-    rename(file, tmp);
-    free(tmp);
+  if(ext) {
+    char n[10];
+    strcpy(n, ext);
+    ext = "";
+    for (size_t i = 0; i < strlen(n); i++) {
+      n[i] = tolower(n[i]);
+    }
+    ext = n;
   }
+  if(!ext || ext == file) ext = ".Unknown";
+  mkdir(ext + 1, 0755);
+  size_t len = strlen(ext + 1) + strlen(file) + sizeof("/");
+  char *tmp = malloc(len);
+  sprintf(tmp, "%s/%s", ext + 1, file);
+  rename(file, tmp);
+  free(tmp);
+}
+void mv2(char *file, char *path) {
+  char *ext = strrchr(file, '.');
+  if(ext) {
+    char n[10];
+    strcpy(n, ext);
+    ext = "";
+    for (size_t i = 0; i < strlen(n); i++) {
+      n[i] = tolower(n[i]);
+    }
+    ext = n;
+  }
+  if(!ext || ext == file) ext = ".Unknown";
+  mkdir(ext + 1, 0755);
+  size_t len = strlen(ext + 1) + strlen(file) + sizeof("/");
+  char *tmp = malloc(len);
+  sprintf(tmp, "%s/%s", ext + 1, file);
+  size_t len2 = strlen(path) + strlen(file) + sizeof("/");
+  char *tmp2 = malloc(len2);
+  sprintf(tmp2, "%s/%s", path, file);
+  rename(tmp2, tmp);
+  free(tmp);
 }
 int main(int argc, char *argv[]) {
-  if(!strcmp(argv[2], "*")) {
+  if (!strcmp(argv[1], "*")) {
+    DIR *d;
     struct dirent *dir;
-    DIR *d = opendir(".");
-    if (d == NULL) {
-      printf("Could not open current directory" );
-      return 0;
-    }
-
-    while ((dir = readdir(d)) != NULL) {
-      if (dir->d_type == DT_REG)
-        printf("%s\n", dir->d_name);
-    }
-
+    d = opendir(".");
+    if (d) {
+      while ((dir = readdir(d)) != NULL) {
+        if (dir->d_type == DT_REG) {
+          mv(dir->d_name);
+        };
+      }
     closedir(d);
+    }
   } else if (!strcmp(argv[1], "-d")) {
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(argv[2]);
+    if (d) {
+      while ((dir = readdir(d)) != NULL) {
+        if (dir->d_type == DT_REG) {
+          mv2(dir->d_name, argv[2]);
+        };
+      }
+    closedir(d);
+    }
   } else if (!strcmp(argv[1], "-f")) {
       for(int i = 2; i < argc; i++) {
           mv(argv[i]);

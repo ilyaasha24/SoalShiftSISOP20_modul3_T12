eb05773efd8cbd24fefb5477f50a5ad6b499df2b
Ilya Asha <ilyaasha24@gmail.com>
2020-04-11 22:06:19 +0700

    Update soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -2,79 +2,65 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <pthread.h>
 #include <dirent.h>
 #include <ctype.h>
-
-void mv(char *file) {
+struct args {
+    char* file;
+    char* path;
+};
+void *mv(void *pass) {
+  char *file = ((struct args*)pass)->file, *path = ((struct args*)pass)->path;
   char *ext = strrchr(file, '.');
-  if(ext) {
+  if (ext) {
     char n[10];
     strcpy(n, ext);
-    ext = "";
-    for (size_t i = 0; i < strlen(n); i++) {
-      n[i] = tolower(n[i]);
-    }
+    for (size_t i = 0; i < strlen(n); i++) n[i] = tolower(n[i]);
     ext = n;
   }
-  if(!ext || ext == file) ext = ".Unknown";
+  if (!ext || ext == file || !strcmp(ext + 1, "*")) ext = ".Unknown";
   mkdir(ext + 1, 0755);
-  size_t len = strlen(ext + 1) + strlen(file) + sizeof("/");
-  char *tmp = malloc(len);
+  size_t len = strlen(ext + 1) + strlen(file) + sizeof("/"), len2 = strlen(path) + strlen(file) + sizeof("/");
+  char *tmp = malloc(len), *tmp2 = malloc(len2);
   sprintf(tmp, "%s/%s", ext + 1, file);
-  rename(file, tmp);
-  free(tmp);
-}
-void mv2(char *file, char *path) {
-  char *ext = strrchr(file, '.');
-  if(ext) {
-    char n[10];
-    strcpy(n, ext);
-    ext = "";
-    for (size_t i = 0; i < strlen(n); i++) {
-      n[i] = tolower(n[i]);
-    }
-    ext = n;
-  }
-  if(!ext || ext == file) ext = ".Unknown";
-  mkdir(ext + 1, 0755);
-  size_t len = strlen(ext + 1) + strlen(file) + sizeof("/");
-  char *tmp = malloc(len);
-  sprintf(tmp, "%s/%s", ext + 1, file);
-  size_t len2 = strlen(path) + strlen(file) + sizeof("/");
-  char *tmp2 = malloc(len2);
   sprintf(tmp2, "%s/%s", path, file);
   rename(tmp2, tmp);
   free(tmp);
 }
 int main(int argc, char *argv[]) {
-  if (!strcmp(argv[1], "*")) {
+  if (argc < 2) perror("Error: Missing Arguments");
+  pthread_t tid[100];
+  if (!strcmp(argv[1], "*") || !strcmp(argv[1], "-d")) {
     DIR *d;
     struct dirent *dir;
-    d = opendir(".");
-    if (d) {
-      while ((dir = readdir(d)) != NULL) {
-        if (dir->d_type == DT_REG) {
-          mv(dir->d_name);
-        };
-      }
-    closedir(d);
-    }
-  } else if (!strcmp(argv[1], "-d")) {
-    DIR *d;
-    struct dirent *dir;
+    if (!strcmp(argv[1], "*")) argv[2] = ".";
     d = opendir(argv[2]);
     if (d) {
+      int i = 0;
       while ((dir = readdir(d)) != NULL) {
-        if (dir->d_type == DT_REG) {
-          mv2(dir->d_name, argv[2]);
+        if (dir->d_type == DT_REG && strcmp(dir->d_name, "soal3")) {
+          struct args *pass = (struct args *)malloc(sizeof(struct args));
+          pass->file = dir->d_name;
+          pass->path = argv[2];
+          pthread_t tid[i];
+          pthread_create(&tid[i++], NULL, &mv, (void *)pass);
         };
       }
+      for (i; i > 0; i--) pthread_join(tid[i], NULL);
     closedir(d);
     }
   } else if (!strcmp(argv[1], "-f")) {
-      for(int i = 2; i < argc; i++) {
-          mv(argv[i]);
-      }
+    if (argc < 3) perror("Error: Missing Arguments");
+    pthread_t tid[argc-2];
+    for (int i = 2; i < argc; i++) {
+      struct args *pass = (struct args *)malloc(sizeof(struct args));
+      pass->file = argv[2];
+      pass->path = ".";
+      pthread_create(&tid[i-2], NULL, &mv, (void *)pass);
+    }
+    for (int i = 2; i < argc; i++) pthread_join(tid[i-2], NULL);
+  } else {
+    perror("Error: Undefined Arguments");
   }
   return 0;
 }
